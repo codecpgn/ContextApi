0c7ef6954d733b19fde9d8b85b1b4bc02f6dafb4
surya <cpgnsc@gmail.com>
2022-10-03 22:31:28 +0600

    firstb commit

---
+++ src/CompC.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FirstName , LastName } from './App'
+const CompC = () => {
+  return (
+    <div>
+    <FirstName.Consumer>{(fname) =>{
+      return(
+        <LastName.Consumer>
+          {(lname) =>{
+            return(
+              <h1 style={{"color":"green"}}>my name is {fname} {lname} </h1>
+            );
+          }}
+        </LastName.Consumer>
+      );
+    } }
+    </FirstName.Consumer>
+    </div>
+  );
+};
+
+export default CompC
