70b0c9009a718fd0f1d31f9f87abf324df2dbe1a
surya <cpgnsc@gmail.com>
2022-10-19 09:13:58 +0600

    #54 changing value of website on button click

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { createContext } from 'react'
 import CompA from './CompA'
 
-const FirstName = createContext();
-const LastName = createContext();
-
 const App = () => {
   return (
     <div>
-    <FirstName.Provider value={"surya"}>
-    <LastName.Provider value={"chapai"}>
       <CompA/>
-    </LastName.Provider>
-    </FirstName.Provider>
-      
     </div>
   )
 }
 export default App
-export {FirstName,LastName};
--- src/CompA.jsx
+++ src/CompA.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import CompB from './CompB'
+import React, { useEffect, useState } from 'react'
 const CompA = () => {
+
+  const [num, setnum] = useState(0);
+  const [nums, setnums] = useState(0);
+
+  useEffect(()=>{
+   // alert("i am clicked");
+   document.title = `you clicked me ${num} times`
+
+  
+  });
   return (
     <div>
-    <CompB/>
+  <button onClick={()=>{
+    setnum(num+1)
+  }
+  }>
+Click Me {num}
+  </button>
+  <br/>
+  <button onClick={
+    (()=>{
+      setnum(num+1);
+    })
+  }>
+Click Me {num}
+  </button>
+  
+
     </div>
   )
 }
